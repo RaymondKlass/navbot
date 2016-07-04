a2b0da1c8de250852c93bd74f1dc86a6081b4e14
Raymond Klass <raymond@klassphoto.com>
2016-07-04 16:02:21 -0400

    Add Setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="navbot",
+      version="0.0.01",
+      author="Raymond Klass",
+      author_email="raymondklass@gmail.com",
+      description="Nav Bot",
+      packages=["navbot"],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+      ],
+      url="https://github.com/raymondklass/navbot"
+)
